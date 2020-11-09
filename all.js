94a9716eb6c38f0a68382e7551aa0d0899bda35f
leave3310 <sampisam3@gmail.com>
2020-11-09 18:42:52 +0800

    lab-29

--- all.js
+++ all.js
@@ -1,15 +1,43 @@
+var info;
+var list = document.querySelector('.list');
+var num = document.querySelector('.choose');
 
 axios
   .get(
     "https://raw.githubusercontent.com/hexschool/hexschoolNewbieJS/master/data.json"
   )
   .then(function (response) {
-    let info = response.data;
-    info.sort(function(a,b){
-      return a.process < b.process
-    })    
-    
-    info.forEach(function(item, index){
-      console.log("第 "+index+" 名是 "+item.name+"，他的特訓班完成度是 "+item.process);
-    })
+    info = response.data;
+
   });
+
+function updateList(e){
+  let select = e.target.value;
+  let str = "";
+  let tmp = info;
+  if(select === 'id'){
+    tmp.sort(function(a,b){
+      return parseInt(a.id) > parseInt(b.id)
+    })
+    tmp.forEach(function(item){
+      return str+= '<li>編號 ID '+ item.id + ' 為 '+item.name+' ，他的特訓班完成度是 '+item.process+'</li>'
+    })
+  }else if(select === 'process'){
+    tmp.forEach(function(item){
+      item.process = parseInt(item.process.substring(0,item.process.length-1));
+      console.log(item.process);
+    })
+    
+    
+    tmp.sort(function(a,b){
+      return a.process < b.process
+    })
+    tmp.forEach(function(item,index){
+      return str+= '<li>第 '+ (index+1) + '名是 '+item.name+' ，他的特訓班完成度是 '+item.process+' %</li>'
+    })
+  }
+  list.innerHTML = str;
+}
+
+num.addEventListener('change', updateList)
+
